19b60fb20b4c4173626d75cd014cec76896e0e01
Daniel <ddemarco03@outlook.com>
2024-06-04 14:54:16 -0700

    initial commit 2

---
+++ pokeman/src/coffeestand/PokemanMain.java
@@ -0,0 +1,48 @@
+package coffeestand;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Stack;
+
+public class PokemanMain {
+
+	/**
+	 * Stores a list of every card in the game
+	 * 
+	 * This will be used to keep an index of every card we add to the game.
+	 * 
+	 * TODO: Replace second "Integer" with type "Card" once implemented
+	 */
+	static final Map<Integer, Integer> CARDS = null;
+	
+	/*
+	 * This will represent the list of cards currently in each player's hand
+	 */
+	static final ArrayList<Integer> PLAYER1_HAND = null;
+	static final ArrayList<Integer> PLAYER2_HAND = null;
+
+	public static void main(String[] args) {
+		/**
+		 * Here we'd have the user prompts, maybe ask how many Pokemon per player, how
+		 * big a stack size, whatever our game opts for. Then we'd initialize the card
+		 * stack with initDeck
+		 * 
+		 * Afterwards, the game will ask the first player to make a move, the second
+		 * player to make a move, and so on until a winning condition is met.
+		 */
+	}
+
+	static void initDeck(Stack<Integer> deck) {
+		// TODO: Replace type Integer with type Card once written
+
+		/*
+		 * Here the card deck will be initialized. It could be set up to have a specific
+		 * arrangement of cards, or randomize based on the list of cards we have
+		 * available. We won't need a return type because the Object class in Java is
+		 * "pass-by-reference", not "pass-by-value" like primitive types, so any
+		 * modifications to "deck" in this method will persist outside of initDeck
+		 */
+	}
+	
+	
+}
